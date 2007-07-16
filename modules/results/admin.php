d514a47b5d2ea7b5d7f4d91a76520e1c3d6281b7
scott-t <scott@smoothsailingcms.org>
2007-07-16 06:54:50 +0000

    New: Race results module - initial import

---
+++ modules/results/admin.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * Race results admin
+ *
+ * Perform administration on race results
+ * @package SSC
+ * @copyright Copyright (c) Scott Thomas
+ * @licence GNU GPL
+ */
+ 
+/**
+ * Check for legit call
+ */
+defined('_VALID_SSC') or die('Restricted access');
+
+echo '<img class="panel-icon-img" src="', $sscConfig_adminImages, '/events.png" alt="" /><span class="title">Race Results</span><hr class="admin" /><div class="indent">';
+
+if(false){
+	
+}else{
+	//display list of race series'
+	
+}
+
+echo '</div>';
+?>
---
+++ modules/results/index.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Race results frontend
+ *
+ * Front-end display of race results
+ * @package SSC
+ * @copyright Copyright (c) Scott Thomas
+ * @licence GNU GPL
+ */
+ 
+/**
+ * Check for legit call
+ */
+defined('_VALID_SSC') or die('Restricted access');
+
+
+?>
