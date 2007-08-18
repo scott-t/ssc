ec7a8163f8dd702db89a4e765c94e4b03d85d952
scott-t <scott@smoothsailingcms.org>
2007-08-18 11:17:35 +0000

    Fix: IE6 Borkedness

--- modules/static/index.php
+++ modules/static/index.php
@@ -18,7 +18,9 @@ $database->setQuery(sprintf("SELECT content, title FROM #__static WHERE nav_id =
 if($database->query() && $data = $database->getAssoc()){
 	echo '<h1>',$data['title'],'</h1>';
 	include_once($sscConfig_absPath.'/includes/sscEdit.php');
-	echo sscEdit::parseToHTML($data['content']),'<br class="clear" />';
+	echo sscEdit::parseToHTML($data['content']);
+	echo '<!--[if !IE 6]><div class="clear" /></div><![endif]-->';
+	echo '<br class="clear" />';
 
 }else{ echo error("An unexpected error occurred. Please contact the webmaster with the link to this page</br >".$database->getErrorMessage());}
 
