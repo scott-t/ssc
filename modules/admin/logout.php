7a65023ba812120ea09ac2cf340f8e1ba5807680
scott-t <scott@smoothsailingcms.org>
2007-11-26 01:06:36 +0000

    Prepare for V3

--- modules/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-unset($_SESSION['UserFullName'], $_SESSION['UserGroup'], $_SESSION['LoginSuccess'],$_SESSION['UserName'],$_SESSION['LoginTries']);
-defined('_VALID_SSC') or die('Restricted access');
-echo message('You should now be logged out...').'<br /><br />';
-?>
--- modules/error/404.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined('_VALID_SSC') or die('Restricted access');
-?><h1>404 - Page not found</h1>
