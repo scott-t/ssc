ff041d20a542259d6b04bd67e4c9c99ab604e4fa
scott-t <scott@smoothsailingcms.org>
2007-11-27 10:30:45 +0000

    New: The first code for V3

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * The primary php file.  All page requests come via this file
+ */
+ 
+/**
+ * Check for legit call
+ */
+defined('_VALID_SSC') or die('Restricted access');
+
+
