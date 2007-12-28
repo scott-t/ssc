48fdd90ec06abda86e77be24b0c63c5ceb1c049b
scott-t <scott@smoothsailingcms.org>
2007-12-28 10:22:32 +0000

    New: Initial environment settings

--- config/default.settings.inc.php
+++ config/default.settings.inc.php
@@ -8,3 +8,10 @@
  * Check for legit call
  */
 defined('_VALID_SSC') or die('Restricted access');
+
+$SSC_SETTINGS['database']['engine'] = '';
+$SSC_SETTINGS['database']['host'] = '';
+$SSC_SETTINGS['database']['port'] = '';
+$SSC_SETTINGS['database']['user'] = '';
+$SSC_SETTINGS['database']['password'] = '';
+$SSC_SETTINGS['database']['table'] = '';
