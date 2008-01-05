042c59ca3de055d3f11804357c8fd380f5fb1726
scott-t <scott@smoothsailingcms.org>
2008-01-05 10:40:39 +0000

    New: Added extra functions to be incorporated in various DB engines

--- includes/core.database.inc.php
+++ includes/core.database.inc.php
@@ -25,6 +25,51 @@ abstract class sscAbstractDatabase{
 	 * Free up the database objects
 	 */
 	abstract public function __destruct();
+
+	/**
+	 * Generate a SELECT query
+	 * @param array $structure Structure for the select
+	 * @return string SQL query syntax that was executed
+	 */
+	abstract public function select_from($structure);
+	
+	/**
+	 * Insert a row or rows into a table.
+	 * 
+	 * If array index [0] exists, then may assume multiple rows are to be inserted
+	 * at the same time with the array elements containing arrays of field=>value pairs
+	 * for each row.  If [0] does not exist, assume a single row where $values is a simple
+	 * field=>value array.
+	 *  
+	 * @param string $table Table to insert into
+	 * @param array $values Array of field=>value pairs to insert into the database
+	 */
+	abstract public function insert_row($table, $values);
+	
+	/**
+	 * Delete a row from a table
+	 * @param string $table Table name
+	 * @param array $id Array of field=>value pairs which are used to help identify the
+	 * 					row(s) to delete
+	 * @return string SQL statement issued
+	 */
+	abstract public function delete_row($table, $id);
+	
+	/**
+	 * Update a given row or rows
+	 * 
+	 * If array index [0] exists in $values, then may assume multiple rows are to be updated
+	 * at the same time with the array elements containing arrays of field=>value pairs
+	 * for each row.  If [0] does not exist, assume a single row where $values is a simple
+	 * field=>value array.
+	 * 
+	 * The WHERE condition of the update is specified with a field key set to "where" which
+	 * should contain an array of field=>value pairs for equivalence
+	 * 
+	 * @param string $table Table name to update
+	 * @param array $values Array of field=>values containing what to update 
+	 */
+	abstract public function update_row($table, $values);
 	
 	/**
 	 * Create a table in the database
@@ -32,6 +77,18 @@ abstract class sscAbstractDatabase{
 	 * @return string SQL statement that was generated and executed
 	 */
 	abstract public function create_table($structure);
+	
+	/**
+	 * Permanently remove a table
+	 * @param string $table Table name representing the table to remove
+	 */
+	abstract public function delete_table($table);
+	
+	/**
+	 * Permanently empty a table
+	 * @param string $table Table name representing the table to empty
+	 */
+	abstract public function emtpy_table($table);
 	
 	/**
 	 * Replace appropriate table prefixes based on environment settings
