d3c2262d0e9e0ddab2b8afc78e98da26148f886e
scott-t <scott@smoothsailingcms.org>
2008-01-30 03:33:33 +0000

    Update: Fiddle with templating

---
+++ themes/php/widget.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PHP theme block template
+ * @package SSC
+ * @subpackage Theme
+ */
+
+?><div class="widget">
+	<div class="widget-title"><?php echo $widget[0]; ?></div>
+	<div class="widget-body"><?php echo $widget[1]; ?></div>
+</div>
